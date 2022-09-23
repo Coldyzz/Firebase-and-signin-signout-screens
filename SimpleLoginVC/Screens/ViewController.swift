9c719ef1122d79230128d536d004fa32587ee585
Coldyzz <dkgkpizza@gmail.com>
2022-09-23 18:13:37 +0200

    added logic to textfields

--- SimpleLoginVC/Screens/ViewController.swift
+++ SimpleLoginVC/Screens/ViewController.swift
@@ -8,19 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var whiteView: UIView!
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
-    
+    @IBOutlet weak var errorEmailLabel: UILabel!
+    @IBOutlet weak var errorPasswordLabel: UILabel!
+      
     override func viewDidLoad() {
         super.viewDidLoad()
         
-    let whiteView = whiteView
+        let whiteView = whiteView
         whiteView!.layer.cornerRadius = 16
-       
+        emailField.layer.borderWidth = 0.5
+        emailField.layer.cornerRadius = 8
+        passwordField.layer.borderWidth = 0.5
+        passwordField.layer.cornerRadius = 8
+        
     }
     @IBAction func signinClicked(_ sender: Any) {
+        
+        let optionalEmail = emailField.text
+        if let email = optionalEmail {
+            if email.contains("@") {
+                emailField.layer.borderColor = UIColor.green.cgColor
+            } else {
+                emailField.layer.borderColor = UIColor.red.cgColor
+            }
+        } else {
+            emailField.layer.borderColor = UIColor.red.cgColor
+        }
+        
+        let optionalPassword = passwordField.text
+        if let password = optionalPassword {
+            if password.count >= 6 {
+                emailField.layer.borderColor = UIColor.green.cgColor
+            } else {
+                passwordField.layer.borderColor = UIColor.red.cgColor
+            }
+        } else {
+            passwordField.layer.borderColor = UIColor.red.cgColor
+        }
     }
     
     @IBAction func forgotPasswordClicked(_ sender: Any) {
