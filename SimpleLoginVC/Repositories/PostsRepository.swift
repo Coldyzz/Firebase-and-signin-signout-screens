e45ccd780b2a1787158d315d19ba3a40d7e3a435
Coldyzz <dkgkpizza@gmail.com>
2022-10-04 19:13:35 +0200

    check check

---
+++ SimpleLoginVC/Repositories/PostsRepository.swift
@@ -0,0 +1,20 @@
+//
+//  PostsRepository.swift
+//  SimpleLoginVC
+//
+//  Created by Дима Холод on 04.10.2022.
+//
+
+import Foundation
+
+protocol PostsRepository {
+    
+}
+
+class FirebasePostsRepository {
+    
+}
+
+class DummyPostsRepository {
+    
+}
