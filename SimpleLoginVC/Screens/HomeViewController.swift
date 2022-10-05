8f06cafed21c938fb96f9a605539ef3c2d756d06
Coldyzz <dkgkpizza@gmail.com>
2022-10-05 18:20:20 +0200

    added iboutlet for tableview, created cell with XIB

--- SimpleLoginVC/Screens/HomeViewController.swift
+++ SimpleLoginVC/Screens/HomeViewController.swift
@@ -7,23 +7,27 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var postsTable: UITableView!
+    
+    let postsRepository: PostsRepository = DummyPostsRepository()
+    var posts: [Post] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
+        postsTable.dataSource = self
+        
+        postsRepository.getAll { posts in
+            self.posts = posts
+            postsTable.reloadData()
+        }
     }
-    
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
 }
