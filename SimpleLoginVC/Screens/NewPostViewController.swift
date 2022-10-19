be89ba72cb66c269c67cfcc0cf67bc7f7343f7ef
Coldyzz <dkgkpizza@gmail.com>
2022-10-19 18:16:01 +0200

    added constrains to create/update screens and reset pass

--- SimpleLoginVC/Screens/NewPostViewController.swift
+++ SimpleLoginVC/Screens/NewPostViewController.swift
@@ -23,11 +23,27 @@ class NewPostViewController: UIViewController {
                                               width: 200,
                                               height: 48))
         field.placeholder = "Please Type Title..."
-        field.backgroundColor = .secondarySystemBackground
+        // field.backgroundColor = .secondarySystemBackground
         field.layer.cornerRadius = 8.0
         field.layer.borderWidth = 1.0
         field.layer.borderColor = UIColor.secondaryLabel.cgColor
+        field.translatesAutoresizingMaskIntoConstraints = false
         return field
+    }()
+    lazy var createButton: UIButton = {
+        let createButton = UIButton(type: .system)
+        createButton.frame = CGRect(x: 8,
+                                    y: 175,
+                                    width: 160,
+                                    height: 48)
+        if mode == NewPostMode.create {
+            createButton.setTitle("Create", for: .normal)
+            createButton.addTarget(self, action: #selector(onCreateClicked), for: .touchUpInside)
+        } else {
+            createButton.setTitle("Update", for: .normal)
+            createButton.addTarget(self, action: #selector(onUpdateClicked), for: .touchUpInside)
+        }
+        return createButton
     }()
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,19 +55,17 @@ class NewPostViewController: UIViewController {
         }
         view.addSubview(titleField)
         titleField.text = editPost?.title
-        let createButton = UIButton(type: .system)
-        createButton.frame = CGRect(x: 8,
-                                    y: 175,
-                                    width: 100,
-                                    height: 48)
-        if mode == NewPostMode.create {
-            createButton.setTitle("Create", for: .normal)
-            createButton.addTarget(self, action: #selector(onCreateClicked), for: .touchUpInside)
-        } else {
-            createButton.setTitle("Update", for: .normal)
-            createButton.addTarget(self, action: #selector(onUpdateClicked), for: .touchUpInside)
-        }
         view.addSubview(createButton)
+        titleField.widthAnchor.constraint(equalTo: view.widthAnchor,
+                                          multiplier: 0.8).isActive = true
+        titleField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        titleField.heightAnchor.constraint(equalToConstant: 48).isActive = true
+        titleField.topAnchor.constraint(equalTo: view.topAnchor,
+                                        constant: 100).isActive = true
+        createButton.topAnchor.constraint(equalTo: titleField.bottomAnchor,
+                                          constant: 16).isActive = true
+        createButton.centerXAnchor.constraint(equalTo: titleField.centerXAnchor).isActive = true
+        createButton.widthAnchor.constraint(greaterThanOrEqualToConstant: 60).isActive = true
     }
     @objc func onCreateClicked() {
         guard let title = titleField.text,
--- SimpleLoginVC/Screens/ResetPasswordViewController.swift
+++ SimpleLoginVC/Screens/ResetPasswordViewController.swift
@@ -16,25 +16,40 @@ class ResetPasswordViewController: UIViewController {
                                               height: 48))
         field.placeholder = "Eneter Your Email.."
         field.autocapitalizationType = .none
-        field.backgroundColor = .secondarySystemBackground
+        // field.backgroundColor = .secondarySystemBackground
         field.layer.cornerRadius = 8.0
         field.layer.borderWidth = 1.0
         field.layer.borderColor = UIColor.secondaryLabel.cgColor
+        field.translatesAutoresizingMaskIntoConstraints = false
         return field
     }()
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .systemBackground
-        title = "Reset Password"
-        view.addSubview(titleField)
+    lazy var createButton: UIButton = {
         let createButton = UIButton(type: .system)
+        createButton.translatesAutoresizingMaskIntoConstraints = false
         createButton.frame = CGRect(x: 8,
                                     y: 175,
                                     width: 100,
                                     height: 48)
         createButton.setTitle("Reset Password", for: .normal)
         createButton.addTarget(self, action: #selector(onResetPasswordClicked), for: .touchUpInside)
+        return createButton
+    }()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        title = "Reset Password"
+        view.addSubview(titleField)
         view.addSubview(createButton)
+        titleField.widthAnchor.constraint(equalTo: view.widthAnchor,
+                                          multiplier: 0.8).isActive = true
+        titleField.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        titleField.heightAnchor.constraint(equalToConstant: 48).isActive = true
+        titleField.topAnchor.constraint(equalTo: view.topAnchor,
+                                        constant: 100).isActive = true
+        createButton.topAnchor.constraint(equalTo: titleField.bottomAnchor,
+                                          constant: 16).isActive = true
+        createButton.centerXAnchor.constraint(equalTo: titleField.centerXAnchor).isActive = true
+        createButton.widthAnchor.constraint(greaterThanOrEqualToConstant: 60).isActive = true
     }
     @objc func onResetPasswordClicked() {
         guard let email = titleField.text else {
